ab3ad2a97d9f3dea5e65ebbab248a2eb00c3d067
Tran Phat Trien <trientranphat@gmail.com>
2023-04-16 02:05:54 +0700

    Add navbar and slider

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import "./App.css";
+import Home from "./pages/Home";
 
 function App() {
-  return;
+  return <Home></Home>;
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,20 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://images.unsplash.com/photo-1661030418545-fd307b4c6f16?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=426&q=80",
+    title: "SEIKO",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS."
+  },
+  {
+    id: 2,
+    img: "https://images.unsplash.com/photo-1594576722512-582bcd46fba3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8d2F0Y2hlc3xlbnwwfDF8MHx8&auto=format&fit=crop&w=600&q=60",
+    title: "MAMBA",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS."
+  },
+  {
+    id: 3,
+    img: "https://images.unsplash.com/photo-1522312346375-d1a52e2b99b3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=394&q=80",
+    title: "TISSOT",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS."
+  }
+];
